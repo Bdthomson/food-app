b580c342ca6fa8e95048374369a0685e16401831
Blake Thomson <bdthomso@asu.edu>
2018-11-16 16:47:35 -0700

    Init

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send({ message: "hi" });
+});
+
+app.listen(port, () => console.log("Listening on port", port));
