704a6ba5e4efcd411e096faa03949958d88749d4
Blake Thomson <bdthomso@asu.edu>
2018-11-16 17:09:02 -0700

    test build

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send({ message: "hi" });
+  res.send({ message: "travis" });
 });
 
 app.listen(port, () => console.log("Listening on port", port));
