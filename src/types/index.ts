e8b2cea7bccc9982681b8afdfa40ef86481cfb01
Blake Thomson <bdthomso@asu.edu>
2018-11-16 20:03:29 -0700

    update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,4 @@
-interface XMLHttpRequest {}
+// error TS2304: Cannot find name 'XMLHttpRequest'
+declare interface XMLHttpRequest {}
+// error TS2304: Cannot find name 'Blob'
+declare interface Blob {}
